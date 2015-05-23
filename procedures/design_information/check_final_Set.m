7663a6c8ebf53b7aba9cd014c4c19f0e9cdfcbfd
Bob Spunt <bobspunt@gmail.com>
2015-05-22 17:18:58 -0700

    added procedural info

---
+++ procedures/design_information/check_final_Set.m
@@ -0,0 +1,11 @@
+clear all
+basedir = pwd;
+% im
+% 1 - human
+% 2 - non-human primate
+% 3 - dog
+im{1} = files('all_photo_high_res/F*jpg');
+im{2} = files('all_photo_high_res/M*jpg');
+im{3} = files('all_photo_high_res/D*jpg');
+
+
